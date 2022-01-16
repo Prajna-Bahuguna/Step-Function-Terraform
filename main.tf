a06e7cf1e91a42700e7100e04bb324589a343239
Prajna Bahuguna <68512999+Prajna-Bahuguna@users.noreply.github.com>
2022-01-16 23:44:15 +0530

    Updated main.tf

--- main.tf
+++ main.tf
@@ -2,29 +2,60 @@ provider "aws" {
   region = var.aws_region
 }
 
-resource "aws_sfn_state_machine" "main-workflow" {
-  name     = "main-workflow"
-  role_arn = "${var.aws_iam_role}"
+resource "aws_sfn_state_machine" "simple-workflow" {
+  name     = "simple-workflow"
+  role_arn = var.aws_iam_role
   definition = <<EOF
 {
-  "Comment": "A Hello World example of the Amazon States Language using Pass states",
+  "Comment": "A simple workflow example of the Amazon States Language using 3 Task states with exception handling",
   "StartAt": "Hello",
   "States": {
     "Hello": {
-      "Type": "Pass",
-      "Result": "Hello",
-      "Next": "Hi"
+      "Type": "Task",
+      "Resource": "aws-servive-arn.arn", #Replace resource value with AWS Service ARN
+      "Next": "Hi",
+      "TimeoutSeconds": 120,
+      "Catch" : [{
+        "ErrorEquals" : ["States.ALL"],
+        "ResultPath": "$.error",
+        "Next" : "Exception"
+      }]    
     },
     "Hi": {
-      "Type": "Pass",
-      "Result": "Hi",
-      "Next": "World"
+      "Type": "Task",
+      "Resource": "aws-servive-arn.arn", #Replace resource value with AWS Service ARN
+      "Next": "World",
+      "TimeoutSeconds": 120,
+      "Catch" : [{
+        "ErrorEquals" : ["States.ALL"],
+        "ResultPath": "$.error",
+        "Next" : "Exception"
+      }]    
     },
     "World": {
-      "Type": "Pass",
-      "Result": "World",
-      "End": true
-    }
+      "Type": "Task",
+      "Resource": "aws-servive-arn.arn", #Replace resource value with AWS Service ARN
+      "Next": "Succeeded",
+      "TimeoutSeconds": 120,
+      "Catch" : [{
+        "ErrorEquals" : ["States.ALL"],
+        "ResultPath": "$.error",
+        "Next" : "Exception"    
+      }]
+    },
+      "Exception" : {
+      "Type": "Task",
+      "Resource": "aws-servive-arn.arn",  #Replace resource value with AWS Service ARN      
+      "ResultPath": "$.error.exception_handled",
+      "Next" : "Failed"
+    },
+    "Succeeded" : {
+      "End" : true,
+      "Type" : "Pass"
+    },
+    "Failed": {
+      "Type": "Fail"
+    }  
   }
 }
 EOF
